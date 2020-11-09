fd76d66c0d3c8b4c36e6f2427f8fa59b1983f532
Rodrigo <rodrigo_moreira84@hotmail.com>
2020-11-09 19:55:24 -0300

    adicionado ojwt

--- src/main/java/com/rodrigo/moneyapi/config/ResourceServerConfig.java
+++ src/main/java/com/rodrigo/moneyapi/config/ResourceServerConfig.java
@@ -3,24 +3,35 @@ package com.rodrigo.moneyapi.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
+       /* auth.inMemoryAuthentication()
+                .withUser("admin").password(this.passwordEncoder().encode("{noop}admin"))
+                .roles("ADMIN");*/
+        auth.inMemoryAuthentication()
+                .withUser("rodrigo").password("{noop}123").roles("ADMIN");
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/categorias").permitAll().anyRequest().authenticated().and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .csrf().disable();
     }
 
     @Override
@@ -28,6 +39,13 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    @Bean
+    @Primary
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 // @Override
 // public void configure(ResourceServerSecurityConfigurer resources) throws
 // Exception {
